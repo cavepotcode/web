22209454b6470f5abbec40d3d16e7f2825dac75b
Pablo Ferrari <pabloferrari1789@gmail.com>
2021-08-31 18:18:53 -0300

    what is cavepot

--- src/components/whatIsCavepot/WhatIsCavepot.tsx
+++ src/components/whatIsCavepot/WhatIsCavepot.tsx
@@ -9,6 +9,22 @@ export default function WhatIsCavepot() {
             <div className="title-wrapper">
                 <label className="title">{t("what-is-cavepot.title")}</label>
             </div>
+            <label>{t("what-is-cavepot.text1.part.1")}
+                <span>{t("what-is-cavepot.text1.part.2")}</span>
+                {t("what-is-cavepot.text1.part.3")}
+                <span>{t("what-is-cavepot.text1.part.4")}</span>
+                {t("what-is-cavepot.text1.part.5")}
+            </label>
+            <label>{t("what-is-cavepot.text2.part.1")}
+                <span>{t("what-is-cavepot.text2.part.2")}</span>
+                {t("what-is-cavepot.text2.part.3")}
+                <span>{t("what-is-cavepot.text2.part.4")}</span>
+            </label>
+            <label>{t("what-is-cavepot.text3.part.1")}
+                <span>{t("what-is-cavepot.text3.part.2")}</span>
+            </label>
+
+            <div className="message">{t("what-is-cavepot.message")}</div>
         </div>
     )
 }
