04a9f4f3965975ac8d2ec9b2d144d03eaa09b10a
niichuu <nicoolasguerrero@gmail.com>
2022-01-28 12:10:16 -0300

    Where ahour name come from Inicio OurHistory

---
+++ src/components/ourHistory/OurHistory.tsx
@@ -0,0 +1,7 @@
+export function OurHistory(){
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- src/components/whereNameComeFrom/WhereNameComeFrom.tsx
+++ src/components/whereNameComeFrom/WhereNameComeFrom.tsx
@@ -1,13 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import './WhereNameComeFrom.scss'
 
-export function WhereNameComeFrom(){
+export function WhereNameComeFrom() {
+    const [t] = useTranslation("global");
+
     return (
         <div className="where-name-come-from">
+            {/* VER COMO COMODAR LAS PELOTITAS */}
+            {/* <div className="sphere-wrapper">
+                <div className="sphere-1"></div>
+                <div className="sphere-2"></div>
+            </div> */}
             <div className="text-wrapper">
-                <div className="sphere-wrapper">
-                    <div className="sphere-1"></div>
-                    <div className="sphere-2"></div>
-                </div>
+                <label className="title">{t("name-come-from.title.1")}</label>
+                <label className="textpart">{t("name-come-from.text.part.1")}</label><br />
+                <label className="title">{t("name-come-from.title.2")}</label>
+                <label className="textpart">{t("name-come-from.text.part.2")}</label>
 
             </div>
         </div>
