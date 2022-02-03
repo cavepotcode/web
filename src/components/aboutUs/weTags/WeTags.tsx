21c13edef18a129e4c8adb4b794e9f7b23ec40a0
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-03 13:45:06 -0300

    finalizacion pagina contact us

--- src/components/aboutUs/weTags/WeTags.tsx
+++ src/components/aboutUs/weTags/WeTags.tsx
@@ -1,18 +1,32 @@
 import "./WeTags.scss";
 import dataTags from "./dataTags"
+import { useTranslation } from "react-i18next";
 
 export function WeTags(){
+    const [t] = useTranslation("global");
+    
     return (
         <div className="we-tag-wrapper">
-            {dataTags.map( (obj, index) => {
-                return (
-                    <div className="tag">
-                        <label>
-                            {obj.text}
-                        </label>
-                    </div>
-                );
-            })}
+            <div className="tags">
+                {dataTags.map( (obj, index) => {
+                    return (
+                        <div className="tag">
+                            <label>
+                                {obj.text}
+                            </label>
+                        </div>
+                    );
+                })}
+            </div>
+            
+            <div className="text-wrapper">
+                <label>
+                    {t("we-wanna.text")}
+                </label>
+                <button>
+                    {t("we-wanna.button")}
+                </button>
+            </div>
         </div>
     );
 }
