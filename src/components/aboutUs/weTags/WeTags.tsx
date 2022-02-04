b0d9d22df5c7fd42d0de6a1996835f0dd3f3034c
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-04 19:00:00 -0300

    formateo de etiquetas con estilo negrita

--- src/components/aboutUs/weTags/WeTags.tsx
+++ src/components/aboutUs/weTags/WeTags.tsx
@@ -1,18 +1,15 @@
 import "./WeTags.scss";
-import dataTags from "./dataTags"
-import { useTranslation } from "react-i18next";
-
+import { useTranslation,Trans } from "react-i18next";
 export function WeTags(){
     const [t] = useTranslation("global");
-    
     return (
         <div className="we-tag-wrapper">
             <div className="tags">
-                {dataTags.map( (obj, index) => {
+                {Object.values(t('we-tags', { returnObjects: true })).map( (obj, index) => {
                     return (
                         <div className="tag">
                             <label>
-                                {obj.text}
+                                <Trans> {obj} </Trans>
                             </label>
                         </div>
                     );
