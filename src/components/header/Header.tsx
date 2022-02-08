6088d5811c210dc31af0909b7c2591cb5303ee2c
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-08 11:37:31 -0300

    instalo react-scroll para mejorar links y modifico navegaciones para que no reviente

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AllImages } from '../../helpers';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Link, scroll } from 'react-scroll';
 
 import './Header.scss';
 
@@ -9,6 +10,15 @@ export const Header = () => {
     const [t] = useTranslation("global");
     const [showMenu, setShowMenu] = useState(false);
     const onShowMenu = () => setShowMenu((prev) => !prev);
+
+    function HeaderView() {
+        const location = useLocation();
+        console.log(location.pathname);
+        return location.pathname
+      }
+    function ToTop() {
+        scroll.scrollToTop();
+    }
 
     useEffect(() => {
         const navItems = document.querySelectorAll('.menu-nav__item');
@@ -32,19 +42,52 @@ export const Header = () => {
             <nav className={showMenu ? 'nav open' : 'nav'}>
                 <ul className={showMenu ? 'menu-nav open' : 'menu-nav'}>
                     <li className="menu-nav__item">
-                        <NavLink to="/" className="menu-nav__link">
-                            {t("header.home")}
-                        </NavLink>
+                    {HeaderView() === '/' && <Link 
+                                className="menu-nav__link"
+                                activeClass="active"
+                                to="technologies-changes"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                {t("header.home")}
+                            </Link>} 
+                        {HeaderView() != '/' && 
+                            <NavLink to="/" className="menu-nav__link">
+                                {t("header.home")}
+                            </NavLink>
+                        } 
                     </li>
                     <li className="menu-nav__item">
-                        <a href="/#how-we-do-it" className="menu-nav__link">
+                        {HeaderView() === '/' && <Link 
+                                className="menu-nav__link"
+                                activeClass="active"
+                                to="how-we-do-it"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                {t("header.how.we.do.it")}
+                            </Link>} 
+                        {HeaderView() != '/' && 
+                            <NavLink to="/" className="menu-nav__link">
+                                {t("header.how.we.do.it")}
+                            </NavLink>
+                        } 
+                        
+                        {/* <Link to="/#how-we-do-it" className="menu-nav__link">
                             {t("header.how.we.do.it")}
-                        </a>
+                        </Link> */}
+                        {/* <a href="/#how-we-do-it" className="menu-nav__link">
+                            {t("header.how.we.do.it")}
+                        </a> */}
                     </li>
                     <li className="menu-nav__item">
-                        <a href="projects.html" className="menu-nav__link">
+                        <NavLink to="/projects" className="menu-nav__link">
                             {t("header.projects")}
-                        </a>
+                        </NavLink>
                     </li>
                     <li className="menu-nav__item">
                         <NavLink to="/aboutus" className="menu-nav__link">
@@ -57,14 +100,14 @@ export const Header = () => {
                         </NavLink>
                     </li>
                     <li className="menu-nav__item">
-                        <a href="contact.html" className="menu-nav__link">
+                        <NavLink to="/blog" className="menu-nav__link">
                             {t("header.blog")}
-                        </a>
+                        </NavLink>
                     </li>
                     <li className="menu-nav__item">
-                        <a href="contact.html" className="menu-nav__link">
+                        <NavLink to="/lab" className="menu-nav__link">
                             {t("header.our.lab")}
-                        </a>
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
