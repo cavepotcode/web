af74219a0c7c033e325e3e51fbf9eeda5295b7d3
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-14 12:10:19 -0300

    Mejoras header para que se cierre al hacer click en responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,16 @@ import { Link, scroll } from 'react-scroll';
 
 import './Header.scss';
 
+interface Size {
+    width: number;
+    height: number;
+  }
+
 export const Header = () => {
+    // The size of the window
+
+    const [size, setSize] = useState<Size>({width: window.innerWidth,height: window.innerHeight});
+
     const [t] = useTranslation("global");
     const [showMenu, setShowMenu] = useState(false);
     const onShowMenu = () => setShowMenu((prev) => !prev);
@@ -19,6 +28,16 @@ export const Header = () => {
     function ToTop() {
         scroll.scrollToTop();
     }
+    // This function updates the state thus re-render components
+    const resizeHanlder = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        setSize({
+            width: width,
+            height: height,
+            });
+    };
 
     useEffect(() => {
         const navItems = document.querySelectorAll('.menu-nav__item');
@@ -30,6 +49,13 @@ export const Header = () => {
         }
 
     }, [showMenu])
+    // Listening for the window resize event
+    useEffect(() => {
+        window.onresize = resizeHanlder;
+
+        // You can also use:
+        //window.addEventListener('resize', resizeHanlder);
+    }, []);
 
     return (
         <div id="header" className={showMenu ? 'header open' : 'header'}>
@@ -42,7 +68,21 @@ export const Header = () => {
             <nav className={showMenu ? 'nav open' : 'nav'}>
                 <ul className={showMenu ? 'menu-nav open' : 'menu-nav'}>
                     <li className="menu-nav__item">
-                        {HeaderView() === '/' && 
+                        {HeaderView() === '/' && size.width <= 1024   && 
+                            <Link 
+                                className="menu-nav__link"
+                                activeClass="active"
+                                to="technologies-changes"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                                onClick={onShowMenu}
+                            >
+                                { size.width }
+                            </Link>
+                        } 
+                        {HeaderView() === '/' && size.width > 1024   && 
                             <Link 
                                 className="menu-nav__link"
                                 activeClass="active"
@@ -52,17 +92,36 @@ export const Header = () => {
                                 offset={-70}
                                 duration={500}
                             >
-                                {t("header.home")}
+                                { size.width }
                             </Link>
                         } 
-                        {HeaderView() != '/' && 
+                        {HeaderView() != '/' && size.width <= 1024 && 
+                            <NavLink to="/" className="menu-nav__link" onClick={onShowMenu}>
+                                { size.width }
+                            </NavLink>
+                        } 
+                        {HeaderView() != '/' && size.width > 1024 && 
                             <NavLink to="/" className="menu-nav__link">
-                                {t("header.home")}
+                                { size.width }
                             </NavLink>
                         } 
                     </li>
                     <li className="menu-nav__item">
-                        {HeaderView() === '/' && 
+                        {HeaderView() === '/' && size.width <= 1024  && 
+                            <Link 
+                                className="menu-nav__link"
+                                activeClass="active"
+                                to="how-we-do-it"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                                onClick={onShowMenu}
+                            >
+                                {t("header.how.we.do.it")}
+                            </Link>
+                        } 
+                        {HeaderView() === '/' && size.width > 1024  && 
                             <Link 
                                 className="menu-nav__link"
                                 activeClass="active"
@@ -75,36 +134,67 @@ export const Header = () => {
                                 {t("header.how.we.do.it")}
                             </Link>
                         } 
-                        {HeaderView() != '/' && 
+                        {HeaderView() != '/' && size.width <= 1024 && 
+                            <NavLink to="/" className="menu-nav__link" onClick={onShowMenu}>
+                                {t("header.how.we.do.it")}
+                            </NavLink>
+                        } 
+                        {HeaderView() != '/'  && size.width > 1024 && 
                             <NavLink to="/" className="menu-nav__link">
                                 {t("header.how.we.do.it")}
                             </NavLink>
                         } 
                     </li>
                     <li className="menu-nav__item">
-                        <NavLink to="/projects" className="menu-nav__link">
-                            {t("header.projects")}
-                        </NavLink>
+                        {size.width <= 1024 && 
+                            <NavLink to="/projects" className="menu-nav__link" onClick={onShowMenu}>
+                                    {  t("header.projects")}
+                            </NavLink> }
+                        {size.width > 1024 && 
+                            <NavLink to="/projects" className="menu-nav__link">
+                                    {  t("header.projects")}
+                            </NavLink> }
                     </li>
                     <li className="menu-nav__item">
-                        <NavLink to="/aboutus" className="menu-nav__link">
-                            {t("header.about.us")}
-                        </NavLink>
+                        {size.width <= 1024 && 
+                            <NavLink to="/aboutus" className="menu-nav__link" onClick={onShowMenu}>
+                                    {  t("header.about.us")}
+                            </NavLink> }
+                        {size.width > 1024 && 
+                            <NavLink to="/aboutus" className="menu-nav__link">
+                                    {  t("header.about.us")}
+                            </NavLink> }
                     </li>
                     <li className="menu-nav__item">
-                        <NavLink to="/contact" className="menu-nav__link">
-                            {t("header.contact.us")}
-                        </NavLink>
+                        {size.width <= 1024 && 
+                            <NavLink to="/contact" className="menu-nav__link" onClick={onShowMenu}>
+                                    {  t("header.contact.us")}
+                            </NavLink> }
+                        {size.width > 1024 && 
+                            <NavLink to="/contact" className="menu-nav__link">
+                                    {  t("header.contact.us")}
+                            </NavLink> }
                     </li>
                     <li className="menu-nav__item">
-                        <NavLink to="/blog" className="menu-nav__link">
-                            {t("header.blog")}
-                        </NavLink>
+                        {size.width <= 1024 && 
+                            <NavLink to="/blog" className="menu-nav__link" onClick={onShowMenu}>
+                                    {  t("header.blog")}
+                            </NavLink> }
+                        {size.width > 1024 && 
+                            <NavLink to="/blog" className="menu-nav__link">
+                                    {  t("header.blog")}
+                            </NavLink> }
                     </li>
                     <li className="menu-nav__item">
-                        <NavLink to="/lab" className="menu-nav__link">
-                            {t("header.our.lab")}
-                        </NavLink>
+                        {size.width <= 1024 && 
+                            <NavLink to="/lab" className="menu-nav__link" onClick={onShowMenu}>
+                                    {  t("header.our.lab")}
+                            </NavLink> }
+                        {size.width > 1024 && 
+                            <NavLink to="/lab" className="menu-nav__link">
+                                    {  t("header.our.lab")}
+                            </NavLink> }
+                       
                     </li>
                 </ul>
             </nav>
