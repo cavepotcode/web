ee0f98db9cef2b776f2502fdc6020bc8e694c148
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-22 17:18:02 -0300

    falta por hacerlo responsive

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,7 +1,47 @@
-export function ProjectCard(){
+import './ProjectCard.scss';
+
+export interface ProjectCardProps{
+    height: number;
+    width: number;
+    img: string;
+    imgX: number;
+    imgY: number;
+    title: string;
+    text: string;
+    textX: number;
+    textY: number;
+    color: string;
+    gridArea: string;
+}
+
+
+export function ProjectCard( {height,width,img,imgX,imgY,title,text,textX,textY, color, gridArea} : ProjectCardProps){
     return(
-        <div>
-            
+        <div className="project-card"
+            style={{
+                gridArea: gridArea,
+                height: height+"px",
+                width: width+"px",
+                backgroundColor: color}}
+        >
+            <div className='title'>
+                <label>{title}</label>
+            </div>
+            <div className='img-wrapper'
+                style={{
+                    left: imgX,
+                    top: imgY}}
+            >
+                <img 
+                    src={require(`../../assets/img/projects/${img}.png`).default} alt="" />
+            </div>
+            <div className='text-wrapper'
+                style={{
+                    left: textX,
+                    top: textY}}
+            >
+                <label>{text}</label>
+            </div>
         </div>
     );
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,34 @@
+import "./Projects.scss";
+import { useTranslation } from 'react-i18next';
+import { ProjectCard, ProjectCardProps } from "./ProjectCard";
+
 export function Projects(){
+    const [t] = useTranslation("global");
+    const projects = Object.values(t('projects.projects', { returnObjects: true }));
+
     return(
-        <div>
+        <div className="projects">
+            <div className="textwrapper">
+                <label className="title">{t("projects.title")}</label>
+
+            </div>
+            <div className="project-card-wrapper">
+                {
+                    projects.map((obj : ProjectCardProps,key) => (
+                        <ProjectCard height={obj.height}
+                                    width={obj.width}
+                                    img={obj.img}
+                                    imgX={obj.imgX}
+                                    imgY={obj.imgY}
+                                    title={obj.title}
+                                    text={obj.text}
+                                    textX={obj.textX}
+                                    textY={obj.textY}
+                                    color={obj.color}
+                                    gridArea={obj.gridArea}></ProjectCard>
+                    ))
+                }
+            </div>
         </div>
         
     );
