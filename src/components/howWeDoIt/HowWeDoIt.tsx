7bfc4f9fef41463a0f76f48a54b57e96773da4e6
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-02-24 14:28:30 -0300

    agrego imagen a how do we do it

--- src/components/howWeDoIt/HowWeDoIt.tsx
+++ src/components/howWeDoIt/HowWeDoIt.tsx
@@ -1,26 +1,28 @@
 import { useTranslation, Trans } from 'react-i18next';
+import { AllImages } from '../../helpers';
 import './HowWeDoIt.scss';
 export const HowWeDoIt = () => {
     const [t] = useTranslation("global");
 
     return (
         <div id="how-we-do-it" className="how-we-do-it">
-            <label className="title">{t("how-we-do-it.title")}</label>
-            <div className="text-wrapper">
-                <label className='textpart'>
-                    <Trans>{t("how-we-do-it.text1")}</Trans>
-                </label>
-                <label  className='textpart'>
-                    <Trans>{t("how-we-do-it.text2")}</Trans>
-                </label>
-                <label  className='textpart'>
-                    <Trans>{t("how-we-do-it.text3")}</Trans>
-                </label>
-                <label  className='textpart'>
-                    <Trans>{t("how-we-do-it.text4")}</Trans>
-                </label>
+            <div className='txt-how-we-do-it'>
+                <label className="title">{t("how-we-do-it.title")}</label>
+                <div className="text-wrapper">
+                    <label className='textpart'>
+                        <Trans>{t("how-we-do-it.text1")}</Trans>
+                    </label>
+                    <label  className='textpart'>
+                        <Trans>{t("how-we-do-it.text2")}</Trans>
+                    </label>
+                    <label  className='textpart'>
+                        <Trans>{t("how-we-do-it.text3")}</Trans>
+                    </label>
+                    <label  className='textpart'>
+                        <Trans>{t("how-we-do-it.text4")}</Trans>
+                    </label>
+                </div>
             </div>
-            
         </div>
     )
 }
