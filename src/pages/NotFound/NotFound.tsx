2d287a69c0cb2b74661130295293aa1355f0c2fa
Nicolas Guerrero <nicolas.guerrero@cavepot.com>
2022-03-03 14:34:17 -0300

    pagina not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,17 @@
+import { useTranslation } from "react-i18next";
+import { AllImages } from "../../helpers";
+
+
 import "./NotFount.scss"
 
 export function NotFoundPage(){
+    const [t] = useTranslation("global");
+    
     return(
         <div className="not-found">
-            404 - NOT FOUNT
+            <label>404</label>
+            <label>NOT FOUND</label>
+            <img src={AllImages.Olla} alt={t("how-we-do-it.alt.img.1")} />
         </div>
     );
 }
