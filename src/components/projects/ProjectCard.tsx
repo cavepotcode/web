cbef98d0e62dfb04ceee9de09211e758967872b6
Bruno Argenta <bruno.argenta@cavepot.com>
2022-04-26 14:54:26 -0300

    change project link to navlink

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -4,7 +4,7 @@ import './ProjectCard.scss';
 import { NavLink } from 'react-router-dom';
 import { url } from 'inspector';
 
-export interface ProjectCardProps{
+export interface ProjectCardProps {
     img: string;
     imgX: number;
     imgY: number;
@@ -20,9 +20,9 @@ export interface ProjectCardProps{
 
 
 
-export function ProjectCard( {img,imgX,imgY,title,text,textX,textY, color,w, gridArea, link} : ProjectCardProps){
+export function ProjectCard({ img, imgX, imgY, title, text, textX, textY, color, w, gridArea, link }: ProjectCardProps) {
 
-     const [size, setSize] = useState<Size>({width: window.innerWidth,height: window.innerHeight});
+    const [size, setSize] = useState<Size>({ width: window.innerWidth, height: window.innerHeight });
 
 
     const resizeHanlder = () => {
@@ -32,19 +32,20 @@ export function ProjectCard( {img,imgX,imgY,title,text,textX,textY, color,w, gri
         setSize({
             width: width,
             height: height,
-            });
+        });
     };
-    
+
     useEffect(() => {
         window.onresize = resizeHanlder;
     }, []);
-    
 
-    return(
+
+    return (
         <div className="project-card"
             style={{
                 gridArea: size.width <= 480 && gridArea === 'auto /span 2' ? "auto /span 1" : gridArea,
-                backgroundColor: color}}
+                backgroundColor: color
+            }}
         >
             <div className='title'>
                 <label>{title}</label>
@@ -55,25 +56,26 @@ export function ProjectCard( {img,imgX,imgY,title,text,textX,textY, color,w, gri
                     top: imgY,
                     width: `${w}%`,
                     backgroundImage: `url(${require(`../../assets/img/projects/${img}.png`).default})`
-                    
+
                 }
                 }
             >
-             
+
             </div>
             <div className='text-wrapper'
                 style={{
                     left: textX,
-                    top: textY}}
+                    top: textY
+                }}
             >
                 <label>{text}</label>
             </div>
             <div className='card-footer'>
-                <a href={link? link: `/project/${img}`} target="_blank">
+                <NavLink to={link ? link : `/project/${img}`} target="_blank">
                     <button>
                         +
                     </button>
-                </a>
+                </NavLink>
             </div>
         </div>
     );
