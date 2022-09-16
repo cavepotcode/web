405df53552575ef1c5483876b1e17e45236e4f08
Bruno Argenta <bruno.argenta@cavepot.com>
2022-09-16 10:55:23 -0300

    change some texts

--- src/components/slider/dataSlider.js
+++ src/components/slider/dataSlider.js
@@ -3,23 +3,33 @@ import { v4 as uuidv4 } from "uuid";
 const dataSlider = [
   {
     id: uuidv4(),
-    text:"Cavepot has made significant contributions to the client's app launches. They assisted in taking the app from ideation to full development. The workflow was extremely effective, as they're good listeners who aren't afraid to share their insights. They're an energetic team that's eager to help",
-    firm:"Fincloud CEO"
+    text:"Cavepot delivered our project in record time, with a high level of care and professionalism. After our first meeting we already knew it was a serious team that knows what is doing and that is also willing to listen different opinions. They adapted to the changes that appeared along the way, and always had the same predisposition and excellent execution. If you are looking for a very complete team that is going to create in your product and take it to the next level, Cavepot is without doubt a great election.",
+    firm:"D. Fiorentin - CEO at Squadability"
+  },
+  {
+    id: uuidv4(),
+    text:"Cavepot helped us facilitate a critical service for our very first client. The team’s dedication went beyond our expectations, they are extremely professional and capable of solving any presented issue. Cavepot´s team took up this challenge, being always ready to adjust to the new project requirements and finding the best solutions to our needs, helping us give a very successful closure to the process.",
+    firm:"J. Jackson - Consultant at Interamericana de Cómputos S.A."
+  },
+  {
+    id: uuidv4(),
+    text:"Arkano Software had the possibility to work together with Cavepot on complex projects from technical and business point of view, creating hight specialized teams, achieving excellent results with hight quality for the client.",
+    firm:"J Suarez - CEO at Arkano Software."
   },
   {
     id: uuidv4(),
     text:"The webpage was featured in a presentation for investors. Since it was launched, more people follow the company on social media. Cavepot communicated well with their partners and completed everything on time. They were efficient and professional.",
-    firm:"Martin Jackson - Director at NewEra"
+    firm:"M. Jackson - Director at NewEra"
   },
   {
     id: uuidv4(),
     text:"The team produced a solid platform that attracts potential buyers, and which bankers appreciate thanks to all the information it collects. Cavepot feels like part of the team rather than a one-off vendor, using their modern ideas and knowledgeable staff to produce high-quality deliverables.",
-    firm:"Sofia Lanza - Co-Founder at Socius"
+    firm:"S. Lanza - Co-Founder at Socius"
   },
   {
     id: uuidv4(),
-    text:"Cavepot delivered an extremely appealing website that also boasts robust functionalities. Its exceptional backend has allowed the client to improve their customer support capabilities, helping them grow as a company.  The talented team managed the project in a communicative, collaborative fashion.",
-    firm:"Giannina Ceruti - Co-Founder at RCD"
+    text:"Work with Cavepot had been a excellent experience, their team were highly commitment with our products, with a high level of effort and quality.",
+    firm:"JP Eguiguren - CTO at DynamizeIT"
   }
 ];
 
--- src/components/whatIsCavepot/WhatIsCavepot.tsx
+++ src/components/whatIsCavepot/WhatIsCavepot.tsx
@@ -9,25 +9,23 @@ export default function WhatIsCavepot() {
         <div className="what-is-cavepot">
             <div className="title-wrapper">
                 <label className="title">{t("what-is-cavepot.title")}</label>
+                {/* <label><Trans>{t("what-is-cavepot.text1")}</Trans></label> */}
+                <label><Trans>{t("what-is-cavepot.text2")}</Trans></label>
+                {/* <label><Trans>{t("what-is-cavepot.text3")}</Trans></label> */}
             </div>
             {/* <div  id="blue-line"></div> */}
             <div className="wrapper">
-                <label>
-                    <Trans>{t("what-is-cavepot.text1")}</Trans>
-                </label>
-                <label>
-                    <Trans>{t("what-is-cavepot.text2")}</Trans>
-                </label>
-                <label>
-                    <Trans>{t("what-is-cavepot.text3")}</Trans>
-                </label>
+
+
+                <div className="message-container">
+                    <NavLink to="/contact">
+                        <button>{t("what-is-cavepot.message")}</button>
+                    </NavLink>
+                </div>
             </div>
             {/* <div id="short-blue-line"></div> */}
-            <div className="message-container">
-                <NavLink to="/contact">
-                    <button>{t("what-is-cavepot.message")}</button>
-                </NavLink>
-            </div>
+
         </div>
+
     )
 }
